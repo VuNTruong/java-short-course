03e1c9c40b4407d0695f8faae22e76078defcb19
VuTruong <truongnguyenanhvu1999@gmail.com>
2025-03-12 00:59:46 +0700

    Added sample

--- src/main/java/com/fpt/taxcalculator/service/PersonService.java
+++ src/main/java/com/fpt/taxcalculator/service/PersonService.java
@@ -6,7 +6,10 @@ import com.fpt.taxcalculator.repository.PersonRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class PersonService {
@@ -25,6 +28,24 @@ public class PersonService {
     }
 
     public void createPerson(PersonCreateRequestDTO request) {
+        Map<String, String> errors = new HashMap<>();
+
+        if (Objects.nonNull(request.getFirstName())) {
+            errors.put("firstName", "First name field cannot be null");
+        }
+
+        if (Objects.nonNull(request.getLastName())) {
+            errors.put("lastName", "Last name field cannot be null");
+        }
+
+        if (request.getTaxCode().length() >= 2 && request.getTaxCode().length() <= 10) {
+            errors.put("taxCode", "Tax code should have between 2 and 10 characters");
+        }
+
+        if (request.getIncome() < 0) {
+            errors.put("income", "Income should be a positive number");
+        }
+
         Person person = new Person();
 
         person.setId(request.getId());
